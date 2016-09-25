adab94e9c47524a60d66ddf24b589034566d9fc3
Abhinand-TK <abhinand300@gmail.com>
2016-09-25 16:55:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,13 +76,14 @@ var htmlTemplate= `<html>
 </body>
 </html>
   `;
+  return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createtemplate(articleone));
 });
 
 app.get('/article-two',function(req,res){
