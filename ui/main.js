cee48818b277ba74a9ced86708bbca888d9dd6ee
Abhinand-TK <abhinand300@gmail.com>
2016-09-28 20:11:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,12 +9,12 @@ var marginLeft =0;
 
 function moveright()
 {
-  marginLeft=marginLeft +10;
+  marginLeft=marginLeft +3;
   img.style.marginLeft = marginLeft+'px';
 }
 
 img.onclick=function()
 {
-  var interval = setInterval(moveright,100);  
+  var interval = setInterval(moveright,20);  
     
 };
