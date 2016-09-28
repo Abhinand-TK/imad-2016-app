55096fef08fd98c61219724446e98d5323d2df6c
Abhinand-TK <abhinand300@gmail.com>
2016-09-28 20:03:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,18 @@ console.log('Loaded!');
 
 var element = document.getElementById('text');
 element.innerHTML='Whatever that was never said was never ever enough! ';
+
+var img = document.getElementById('neel');
+var marginleft =0;
+
+function moveright()
+{
+  marginLeft=marginLeft +10;
+  img.style.marginLeft+'px';
+}
+
+img.onclick=function()
+{
+  var interval = setInterval(moveright,100);  
+    
+};
